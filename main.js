56c0990bb0cf01e9a36662ad3e6bc3271849ee8f
József Varga <sikloernyos@hotmail.com>
2021-11-03 08:06:40 +0100

    random fieldcreation with random hat with user measured fielddimension

--- main.js
+++ main.js
@@ -10,16 +10,61 @@ const pathCharacter = "*";
 class Field {
   constructor(field) {
     this.field = field;
+    this.field2;
     this.success = 0;
     this.x = 0;
     this.y = 0;
+    this.height = 3;
+    this.width = 3;
   }
 
+  generateField(h, w, p) {
+    this.field2 = Array.from(Array(h), () => new Array(w));
+    let holeRate = Math.ceil(h * w * (p / 100));
+    //let patternField = [hole, fieldCharacter];
+
+    //--fill up with feldcharacter
+    for (let i = 0; i < this.field2.length; i++) {
+      for (let j = 0; j < this.field2[i].length; j++) {
+        this.field2[i][j] = fieldCharacter;
+      }
+    }
+    //--fill up with hole character
+    do  {
+      let xArray = Math.floor(Math.random() * this.field2.length);
+      let yArray = Math.floor(Math.random() * this.field2[0].length);
+      //--check of pathhcaracterposition
+      if (xArray || yArray) {
+          this.field2[xArray][yArray] = hole;
+      }
+    } while (this.field2.flat().filter(element => element === hole).length < holeRate);
+    console.log(holeRate);
+    //--add path beginn 
+    this.field2[0][0] = pathCharacter;
+    //--add Hat charachter to the random free position
+    let noPass = true;
+    while (noPass) {
+    let xArray2 = Math.floor(Math.random() * this.field2.length);
+    let yArray2 = Math.floor(Math.random() * this.field2[0].length);
+    if (this.field2[xArray2][yArray2] === fieldCharacter) {
+        this.field2[xArray2][yArray2] =  hat;
+        noPass = false;
+    }
+}
+
+
+
+
+    this.field2.forEach((element) => {
+        console.log(...element);
+      });
+      console.log();
+  }
 
   print() {
-    console.log(blank);
-    readline.cursorTo(process.stdout, 0, 0);
-    readline.clearScreenDown(process.stdout);
+    //console.log(blank);
+    //readline.cursorTo(process.stdout, 0, 0);
+    //readline.clearScreenDown(process.stdout);
 
     this.field.forEach((element) => {
       console.log(...element);
@@ -127,7 +172,7 @@ class Field {
               this.reDrawFeld(this.x, this.y);
               break;
             case pathCharacter:
-                pathCharacter.fontcolor('red');
+              pathCharacter.fontcolor("red");
               this.reDrawFeld(this.x, this.y);
               break;
           }
@@ -141,6 +186,7 @@ class Field {
   }
 
   userQuestion() {
+    this.generateField(6, 8, 20);
     this.print();
     while (!this.success) {
       const userDirection = prompt("Which way?");
