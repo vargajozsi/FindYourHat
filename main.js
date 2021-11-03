3b291bfcc7d350c5f8032b3c0a52df31daa68301
József Varga <sikloernyos@hotmail.com>
2021-11-03 21:06:27 +0100

    40 line braclets and let field2

--- main.js
+++ main.js
@@ -10,7 +10,6 @@ const pathCharacter = "*";
 class Field {
   constructor(field) {
     this.field = field;
-    this.field2;
     this.success = 0;
     this.x = 0;
     this.y = 0;
@@ -18,53 +17,52 @@ class Field {
     this.width = 3;
   }
 
-  generateField(h, w, p) {
-    this.field2 = Array.from(Array(h), () => new Array(w));
+  static generateField(h, w, p) {
+    
+    let field2 = Array.from(Array(h), () => new Array(w));
     let holeRate = Math.ceil(h * w * (p / 100));
     //let patternField = [hole, fieldCharacter];
 
     //--fill up with feldcharacter
-    for (let i = 0; i < this.field2.length; i++) {
-      for (let j = 0; j < this.field2[i].length; j++) {
-        this.field2[i][j] = fieldCharacter;
+    for (let i = 0; i < field2.length; i++) {
+      for (let j = 0; j < field2[i].length; j++) {
+        field2[i][j] = fieldCharacter;
       }
     }
     //--fill up with hole character
     do  {
-      let xArray = Math.floor(Math.random() * this.field2.length);
-      let yArray = Math.floor(Math.random() * this.field2[0].length);
+      let xArray = Math.floor(Math.random() * field2.length);
+      let yArray = Math.floor(Math.random() * field2[0].length);
       //--check of pathhcaracterposition
       if (xArray || yArray) {
-          this.field2[xArray][yArray] = hole;
-      }
-    } while (this.field2.flat().filter(element => element === hole).length < holeRate);
-    console.log(holeRate);
+          field2[xArray][yArray] = hole;
+      } else {}
+    } while (field2.flat().filter(element => element === hole).length < holeRate);
     //--add path beginn 
-    this.field2[0][0] = pathCharacter;
+    field2[0][0] = pathCharacter;
     //--add Hat charachter to the random free position
     let noPass = true;
     while (noPass) {
-    let xArray2 = Math.floor(Math.random() * this.field2.length);
-    let yArray2 = Math.floor(Math.random() * this.field2[0].length);
-    if (this.field2[xArray2][yArray2] === fieldCharacter) {
-        this.field2[xArray2][yArray2] =  hat;
+    let xArray2 = Math.floor(Math.random() * field2.length);
+    let yArray2 = Math.floor(Math.random() * field2[0].length);
+    if (field2[xArray2][yArray2] === fieldCharacter) {
+        field2[xArray2][yArray2] =  hat;
         noPass = false;
     }
 }
 
 
 
-
-    this.field2.forEach((element) => {
+    return field2
+    /*field2.forEach((element) => {
         console.log(...element);
       });
-      console.log();
-  }
+    */  }
 
   print() {
-    //console.log(blank);
-    //readline.cursorTo(process.stdout, 0, 0);
-    //readline.clearScreenDown(process.stdout);
+    console.log(blank);
+    readline.cursorTo(process.stdout, 0, 0);
+    readline.clearScreenDown(process.stdout);
 
     this.field.forEach((element) => {
       console.log(...element);
@@ -186,7 +184,6 @@ class Field {
   }
 
   userQuestion() {
-    this.generateField(6, 8, 30);
     this.print();
     while (!this.success) {
       const userDirection = prompt("Which way?");
@@ -195,12 +192,5 @@ class Field {
   }
 }
 
-const myField = new Field([
-  ["*", "░", "O", "O", "^"],
-  ["░", "░", "░", "O", "░"],
-  ["░", "O", "░", "░", "░"],
-  ["░", "░", "░", "O", "░"],
-  ["░", "^", "░", "░", "░"],
-  ["░", "░", "░", "O", "░"],
-]);
+const myField = new Field(Field.generateField(10, 10, 10));
 myField.userQuestion();
